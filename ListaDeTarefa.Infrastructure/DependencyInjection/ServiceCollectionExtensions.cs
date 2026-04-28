3995af376b765a395e2050558640afb618882620
Matheus Henrique Schumliak <matheeus.hs@gmail.com>
2026-04-28 04:13:13 -0300

    Refatoração e adição de repositórios e contexto EF
    Refatorado o caminho da pasta de interfaces no projeto Application.
    Adicionadas referências do Entity Framework Core no projeto Infrastructure.
    Criadas interfaces `IGenericRepository` e `ITarefaRepository`.
    Implementado o contexto `ListaDeTarefaContexto` para EF Core.
    Adicionada classe de extensão para injeção de dependências.
    Definido mapeamento da entidade `Tarefa` com `TarefaMapping`.
    Implementados repositórios genéricos e específicos para `Tarefa`.

---
+++ ListaDeTarefa.Infrastructure/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿
+
+using ListaDeTarefa.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ListaDeTarefa.Infrastructure.DependencyInjection
+{
+	public static class ServiceCollectionExtensions
+	{
+		public static IServiceCollection AddProjectDependencies(this IServiceCollection services, string connectionString)
+		{
+			services.AddDbContext<ListaDeTarefaContexto>(options =>
+				options.UseSqlServer(connectionString));
+
+			services.AddScoped<ITarefaRepository, TarefaRepository>();
+			services.AddScoped<ITarefaService, TarefaService>();
+
+
+			return services;
+		}
+	}
+}
